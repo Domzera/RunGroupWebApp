699b40071b197fd559b83d2f8535e3848df8dde5
Domzera <luizceliogf@hotmail.com>
2022-12-23 15:03:48 -0300

    First commit

---
+++ RunGroupWebApp/Data/Seed.cs
@@ -0,0 +1,115 @@
+﻿using RunGroupWebApp.Data.Enum;
+using RunGroupWebApp.Models;
+using System.Reflection;
+
+namespace RunGroupWebApp.Data
+{
+    public class Seed
+    {
+        public static void SeedData(IApplicationBuilder applicationBuilder)
+        {
+            using (var serviceScope = applicationBuilder.ApplicationServices.CreateScope())
+            {
+                var context = serviceScope.ServiceProvider.GetService<ApplicationDbContext>();
+
+                context.Database.EnsureCreated();
+
+                //Club
+                if (!context.Clubs.Any())
+                {
+                    context.Clubs.AddRange(new List<Club>()
+                    {
+                        new Club()
+                        {
+                            Title = "Running Club 1",
+                            Description = "This is the description of the first cinema",
+                            Image = "https://www.eatthis.com/wp-content/uploads/sites/4/2020/05/running.jpg?quality=82&strip=1&resize=640%2C360",
+                            Address = new Address()
+                            {
+                                Street = "123 Main St",
+                                City = "Charlotte",
+                                State = "NC"
+                            },
+                            ClubCategory = ClubCategory.City
+                         },
+                        new Club()
+                        {
+                            Title = "Running Club 2",
+                            Description = "This is the description of the first cinema",
+                            Image = "https://www.eatthis.com/wp-content/uploads/sites/4/2020/05/running.jpg?quality=82&strip=1&resize=640%2C360",
+                            Address = new Address()
+                            {
+                                Street = "123 Main St",
+                                City = "Charlotte",
+                                State = "NC"
+                            },
+                            ClubCategory = ClubCategory.Endurance
+                        },
+                        new Club()
+                        {
+                            Title = "Running Club 3",
+                            Description = "This is the description of the first club",
+                            Image = "https://www.eatthis.com/wp-content/uploads/sites/4/2020/05/running.jpg?quality=82&strip=1&resize=640%2C360",
+                            Address = new Address()
+                            {
+                                Street = "123 Main St",
+                                City = "Charlotte",
+                                State = "NC"
+                            },
+                            ClubCategory = ClubCategory.Trail
+                        },
+                        new Club()
+                        {
+                            Title = "Running Club 3",
+                            Description = "This is the description of the first club",
+                            Image = "https://www.eatthis.com/wp-content/uploads/sites/4/2020/05/running.jpg?quality=82&strip=1&resize=640%2C360",
+                            Address = new Address()
+                            {
+                                Street = "123 Main St",
+                                City = "Michigan",
+                                State = "NC"
+                            },
+                            ClubCategory = ClubCategory.City
+                        }
+                    });
+                    context.SaveChanges();
+                }
+                //Racess
+                if (!context.Races.Any())
+                {
+                    context.AddRange(new List<Race>()
+                    {
+                        new Race()
+                        {
+                            Title = "Running Race 1",
+                            Image = "https://www.eatthis.com/wp-content/uploads/sites/4/2020/05/running.jpg?quality=82&strip=1&resize=640%2C360",
+                            Description = "This is the description of the first race",
+                            RaceCategory = RaceCategory.Marathon,
+                            Address = new Address()
+                            {
+                                Street = "123 Main St",
+                                City = "Charlotte",
+                                State = "NC"
+                            }
+                        },
+                        new Race()
+                        {
+                            Title = "Running Race 2",
+                            Image = "https://www.eatthis.com/wp-content/uploads/sites/4/2020/05/running.jpg?quality=82&strip=1&resize=640%2C360",
+                            Description = "This is the description of the first race",
+                            RaceCategory = RaceCategory.Ultra,
+                            AddressId = 5,
+                            Address = new Address()
+                            {
+                                Street = "123 Main St",
+                                City = "Charlotte",
+                                State = "NC"
+                            }
+                        }
+                    });
+                    context.SaveChanges();
+                }
+            }
+        }
+    }
+}
