e1633f61ed14c756b629492242fdd938ab03f115
Domzera <luizceliogf@hotmail.com>
2022-12-28 16:48:42 -0300

    Rotas do navegadores funcionando com sub class do modelo

--- RunGroupWebApp/Controllers/RaceController.cs
+++ RunGroupWebApp/Controllers/RaceController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using RunGroupWebApp.Data;
 using RunGroupWebApp.Models;
 
@@ -17,5 +18,12 @@ namespace RunGroupWebApp.Controllers
             List<Race> races = _context.Races.ToList();
             return View(races);
         }
+
+        public IActionResult Detail(int id)
+        {//Nunca esquecer de utilizar o Include quando tiver sub categorias no modelo
+            Race race = _context.Races.Include(a => a.Address).FirstOrDefault(c => c.Id == id);
+            return View(race);
+        }
+
     }
 }
