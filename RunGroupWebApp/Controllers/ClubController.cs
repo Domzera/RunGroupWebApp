248bf5baf2ee5b7d304bdc5c30c4f49eb279be59
Domzera <luizceliogf@hotmail.com>
2022-12-29 16:05:31 -0300

    Com a inserção de novos clubes no banco de dados

--- RunGroupWebApp/Controllers/ClubController.cs
+++ RunGroupWebApp/Controllers/ClubController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using RunGroupWebApp.Data;
+using RunGroupWebApp.Interfaces;
 using RunGroupWebApp.Models;
 
 namespace RunGroupWebApp.Controllers
@@ -8,10 +9,14 @@ namespace RunGroupWebApp.Controllers
     public class ClubController : Controller
     {
         private readonly ApplicationDbContext _context;
-        public ClubController(ApplicationDbContext context)
+        private readonly IClubRepository _clubRepository;
+
+        public ClubController(ApplicationDbContext context,IClubRepository clubRepository)
         {
             _context=context;
+            _clubRepository=clubRepository;
         }
+
         public IActionResult Index()
         {
             List<Club> clubs = _context.Clubs.ToList();
@@ -22,6 +27,21 @@ namespace RunGroupWebApp.Controllers
         {//Nunca esquecer de utilizar o Include quando tiver sub categorias no modelo
             Club club = _context.Clubs.Include(a => a.Address).FirstOrDefault(c => c.Id == id);
             return View(club);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Club club)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(club);
+            }
+            _clubRepository.Add(club);
+            return RedirectToAction("Index");
         }
     }
 }
