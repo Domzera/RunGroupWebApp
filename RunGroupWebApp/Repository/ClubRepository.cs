6fbbd751cf217055b2e095f2a99431e5b08d464a
Domzera <luizceliogf@hotmail.com>
2022-12-29 14:18:56 -0300

    Controlers funcionando com banco de dados

---
+++ RunGroupWebApp/Repository/ClubRepository.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RunGroupWebApp.Data;
+using RunGroupWebApp.Interfaces;
+using RunGroupWebApp.Models;
+
+namespace RunGroupWebApp.Repository
+{
+    public class ClubRepository : IClubRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ClubRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public bool Add(Club club)
+        {
+            //throw new NotImplementedException();
+            _context.Add(club);
+            return Save();
+        }
+
+        public bool Delete(Club club)
+        {
+            //throw new NotImplementedException();
+            _context.Remove(club);
+            return Save();
+        }
+
+        //Aqui por ser ação asincrona tem que ter esses 'async' ' await' e o ToList-Async
+        public async Task<IEnumerable<Club>> GetAll()
+        {
+            //throw new NotImplementedException();
+            return await _context.Clubs.ToListAsync();
+        }
+
+        public async Task<Club> GetByIdAsync(int id)
+        {
+            //throw new NotImplementedException();
+            return await _context.Clubs.FirstOrDefaultAsync(i => i.Id == id);
+        }
+
+        public async Task<IEnumerable<Club>> GetClubByCity(string city)
+        {
+            //throw new NotImplementedException();
+            return await _context.Clubs.Where(c => c.Address.City.Contains(city)).ToListAsync();
+        }
+
+        public bool Save()
+        {
+            //throw new NotImplementedException();
+            var saved = _context.SaveChanges();
+            return saved > 0 ? true : false;
+        }
+
+        public bool Update(Club club)
+        {
+            //throw new NotImplementedException();
+            _context.Update(club);
+            return Save();
+        }
+    }
+}
---
+++ RunGroupWebApp/Repository/RaceRepository.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RunGroupWebApp.Data;
+using RunGroupWebApp.Interfaces;
+using RunGroupWebApp.Models;
+
+namespace RunGroupWebApp.Repository
+{
+    public class RaceRepository:IRaceRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public RaceRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public bool Add(Race race)
+        {
+            //throw new NotImplementedException();
+            _context.Add(race);
+            return Save();
+        }
+
+        public bool Delete(Race race)
+        {
+            //throw new NotImplementedException();
+            _context.Remove(race);
+            return Save();
+        }
+
+        //Aqui por ser ação asincrona tem que ter esses 'async' ' await' e o ToList-Async
+        public async Task<IEnumerable<Race>> GetAll()
+        {
+            //throw new NotImplementedException();
+            return await _context.Races.ToListAsync();
+        }
+
+        public Task<IEnumerable<Race>> GetAllRacesByCity(string city)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Race> GetByIdAsync(int id)
+        {
+            //throw new NotImplementedException();
+            return await _context.Races.FirstOrDefaultAsync();
+        }
+
+        public bool Save()
+        {
+            //throw new NotImplementedException();
+            var saved = _context.SaveChanges();
+            return saved > 0 ? true : false;
+        }
+
+        public bool Update(Race race)
+        {
+            //throw new NotImplementedException();
+            _context.Update(race);
+            return Save();
+        }
+    }
+}
