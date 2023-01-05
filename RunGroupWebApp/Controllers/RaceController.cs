c2324e137ff405ddd141b22dcc04e53547e4800f
Domzera <luizceliogf@hotmail.com>
2023-01-05 16:01:07 -0300

    Upload de fotos funcionando com Cloudinary para clubs e para races

--- RunGroupWebApp/Controllers/RaceController.cs
+++ RunGroupWebApp/Controllers/RaceController.cs
@@ -3,6 +3,9 @@ using Microsoft.EntityFrameworkCore;
 using RunGroupWebApp.Data;
 using RunGroupWebApp.Interfaces;
 using RunGroupWebApp.Models;
+using RunGroupWebApp.Repository;
+using RunGroupWebApp.Services;
+using RunGroupWebApp.ViewModels;
 
 namespace RunGroupWebApp.Controllers
 {
@@ -10,11 +13,13 @@ namespace RunGroupWebApp.Controllers
     {
         private readonly ApplicationDbContext _context;
         private readonly IRaceRepository _raceRepository;
+        private readonly IPhotoService _photoService;
 
-        public RaceController(ApplicationDbContext context, IRaceRepository raceRepository)
+        public RaceController(ApplicationDbContext context, IRaceRepository raceRepository, IPhotoService photoService)
         {
             _context = context;
             _raceRepository = raceRepository;
+            _photoService = photoService;
         }
         public IActionResult Index()
         {
@@ -34,14 +39,32 @@ namespace RunGroupWebApp.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> Create(Race race)
+        public async Task<IActionResult> Create(CreateRaceViewModel raceVM)
         {
-            if (!ModelState.IsValid)
+            if (ModelState.IsValid)
             {
-                return View(race);
+                var result = await _photoService.AddPhotoAsync(raceVM.Image);
+
+                var race = new Race
+                {
+                    Title = raceVM.Title,
+                    Description = raceVM.Description,
+                    Image = result.Url.ToString(),
+                    Address = new Address
+                    {
+                        Street = raceVM.Address.Street,
+                        City = raceVM.Address.City,
+                        State = raceVM.Address.State
+                    }
+                };
+                _raceRepository.Add(race);
+                return RedirectToAction("Index");
             }
-            _raceRepository.Add(race);
-            return RedirectToAction("Index");
+            else
+            {
+                ModelState.AddModelError("", "Photo upload failed");
+            }
+            return View(raceVM);
         }
 
     }
