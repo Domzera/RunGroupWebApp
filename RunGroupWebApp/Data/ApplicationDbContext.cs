49a2b3ba63e4a483cc9645f09afb84925371f865
Domzera <luizceliogf@hotmail.com>
2023-02-14 15:07:59 -0300

    Identity instalado e login funcioando

--- RunGroupWebApp/Data/ApplicationDbContext.cs
+++ RunGroupWebApp/Data/ApplicationDbContext.cs
@@ -1,11 +1,12 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using RunGroupWebApp.Models;
 
 namespace RunGroupWebApp.Data
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext<AppUser> //Se você fizer regras para o identity entã você pode suar <AppUser, AppRoles>
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
 
         }
